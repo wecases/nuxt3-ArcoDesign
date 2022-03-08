4347cfa4271cef6ff89610ae71a6854274bb1735
waset <waset@foxmail.com>
2022-03-08 14:29:52 +0800

    style: 💄 add arco-design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
 import { defineNuxtConfig } from 'nuxt3'
 
+import Components from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { ArcoResolver } from "unplugin-vue-components/resolvers";
+
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    vite: {
+        plugins: [
+            Components({
+                dts: true,
+                resolvers: [
+                    IconsResolver({
+                        prefix: 'i'
+                    }),
+                    ArcoResolver({
+                        importStyle: 'css',
+                        resolveIcons: true
+                    })
+                ],
+            }),
+        ],
+    },
+    build: {
+        transpile: ['compute-scroll-into-view', '@arco-design/web-vue'],
+    },
 })
