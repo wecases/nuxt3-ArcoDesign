89f96fc9a47e7ac13a6b07ef6712af653f4603ce
waset <waset@foxmail.com>
2022-03-10 20:48:24 +0800

    build: 👷 remove build tool

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,7 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-import Components from 'unplugin-vue-components/vite'
-import Icons from 'unplugin-icons/vite'
-import IconsResolver from 'unplugin-icons/resolver'
-import { ArcoResolver } from "unplugin-vue-components/resolvers";
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    vite: {
-        plugins: [
-            Components({
-                dts: true,
-                resolvers: [
-                    IconsResolver({
-                        prefix: 'icon'
-                    }),
-                    ArcoResolver({
-                        resolveIcons: false
-                    })
-                ],
-            }),
-            Icons()
-        ],
-    },
     build: {
         transpile: ['compute-scroll-into-view'],
     },
